f014e170f3b7ca174b6499779e7e353bffb4c602
Eugene <flatline.mail@gmail.com>
2015-10-05 20:06:13 +0500

    Create dice.py

---
+++ dice.py
@@ -0,0 +1,24 @@
+__author__ = 'Flatline01'
+# The simulator throws a two dice that displays the result of the throw
+
+print("With this program you throw two dice!")
+
+# The random number generator throws two dice
+while True:
+    from random import randint
+    dice11 = randint(1, 6)
+    dice12 = randint(1, 6)
+    result1 = dice11 + dice12
+
+    print("--------------------------")
+    print("First Dice:", dice11, "\t\tSecond Dice:", dice12, "\n\t\tYour points:", result1)
+
+
+# The interface to end the program
+    answer1 = ["no", "n", "quit", "bye", "exit"]
+    choose = str(input("\nWant to continue? (enter/no)"))
+
+    # Does the case insensitive
+    choose = choose.lower()
+    if choose in answer1:
+        break
