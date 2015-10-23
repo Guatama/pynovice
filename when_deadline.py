02f92ecba087801f6bfca643d7d39986a76eb337
Eugene <flatline.mail@gmail.com>
2015-10-24 00:50:02 +0500

    Update when_deadline.py

--- when_deadline.py
+++ when_deadline.py
@@ -2,20 +2,26 @@ __author__ = 'Flatline01'
 import datetime
 
 choosenDate = datetime.date.today()
-answer1 = ["no", "n"]
-answer2 = ["yes", "y"]
+answerN = ["no", "n"]
+answerY = ["yes", "y"]
+repeatQuestion = False
 
 print("This program calculate the deadline date,\nif the user enters number of days before that.\n")
-choose = str(input("Do you want to enter a specific date? (Y/N) "))
 
-# Does the case insensitive
-choose = choose.lower()
+while repeatQuestion == False:
+    choose = str(input("Do you want to enter a specific date? (Y/N) "))
 
-if choose in answer1:
-    print("\nToday is " + choosenDate.strftime("%d %B, %Y"))
-elif choose in answer2:
-    inputDate = input("\nEnter the start date (DD/MM/YYYY): ")
-    choosenDate = datetime.datetime.strptime(inputDate, "%d/%m/%Y").date()
+    # Does the case insensitive
+    choose = choose.lower()
+
+    if choose in answerN:
+        repeatQuestion = True
+        print("\nToday is " + choosenDate.strftime("%d %B, %Y"))
+
+    elif choose in answerY:
+        repeatQuestion = True
+        inputDate = input("\nEnter the start date (DD/MM/YYYY): ")
+        choosenDate = datetime.datetime.strptime(inputDate, "%d/%m/%Y").date()
 
 passingDay = datetime.timedelta(days = int(input("Enter the number of days: ")))
 deadlineDay = choosenDate + passingDay
