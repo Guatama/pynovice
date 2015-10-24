574a65d700dbd8ef2d7cefd04722e12534ca4b76
Eugene <flatline.mail@gmail.com>
2015-10-24 10:11:13 +0500

    Update and rename howManyDays to howManyDays.py

--- howManyDays.py
+++ howManyDays.py
@@ -6,7 +6,7 @@ currentDate = datetime.date.today()
 
 print("Here you can calculate how many\ndays are left until a certain date.")
 
-deadlineDate = datetime.datetime.strptime(input("\nEnter date of event: (DD/MM/YY)"), "%d/%m/%Y").date()
+deadlineDate = datetime.datetime.strptime(input("\nEnter date of event (DD/MM/YY): "), "%d/%m/%Y").date()
 numDays = deadlineDate - currentDate
 
 print("\nToday is " + currentDate.strftime('%d %B, %Y'))
