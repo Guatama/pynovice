7222d4ee4afc713047cabb3804c8edc0cf543832
Flatline01 <flatline.mail@gmail.com>
2018-10-26 18:54:46 +0500

    Some small improvement in logic

--- howManyDays.py
+++ howManyDays.py
@@ -7,9 +7,20 @@ currentDate = datetime.date.today()
 print("Here you can calculate how many\ndays are left until a certain date.")
 
 deadlineDate = datetime.datetime.strptime(input("\nEnter date of event (DD/MM/YY): "), "%d/%m/%Y").date()
-numDays = deadlineDate - currentDate
+time_between = deadlineDate - currentDate
+numDays = time_between.days
 
 print("\nToday is " + currentDate.strftime('%d %B, %Y'))
-print("Before the event " + str(numDays.days) + " days left.")
+if numDays > 0:
+    print("Before the event " + str(numDays) + " days left.")
 
-# TODO: add support "negative" date (how much days went after birth date for example)
+elif numDays == 0:
+    print("This event is Today!")
+
+else:
+    numDays *= -1
+    print("After the event, " + str(numDays) + " days have passed.")
+
+
+# TODO: add support "negative" date (how much days went after birth date for example) DONE
+# TODO: add function for calculation parts and simplify the code
