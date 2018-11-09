df1978105366220eb5b1729a4588f8f0ba8dc1aa
Flatline01 <flatline.mail@gmail.com>
2018-11-09 12:36:41 +0500

    TickTackToe python game for comand line (milestone project 1)

---
+++ tictacktoe_game.py
@@ -0,0 +1,146 @@
+__author__ = 'Flatline01'
+JupNB = False
+testGame = ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
+newGame = ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+def clear_screen(JupyterNotebook=False):
+    """
+        Clear screen in terminal. With JupyerNotebook == True - did it for that enviroment with IPython function
+    """ 
+    if JupyterNotebook:
+        from IPython.display import clear_output
+        clear_output()
+    else:
+        print('\n' * 100)
+
+
+def board_display(board):
+    """
+        Displaying board-grid and markers in places in term
+    """
+    spaces = " " * 10
+    line_space = spaces + "    |   |    "
+    line_horiz = spaces + "—" * 13
+
+    clear_screen(JupNB)
+    print(line_space + "\n" + spaces + " " + board[1] + "  | " + board[2] + " | " + board[3] + " ")     # 1 line
+    print(line_space + "\n" + line_horiz)
+    print(line_space + "\n" + spaces + " " + board[4] + "  | " + board[5] + " | " + board[6] + " ")     # 2 line
+    print(line_space + "\n" + line_horiz)
+    print(line_space + "\n" + spaces + " " + board[7] + "  | " + board[8] + " | " + board[9] + " ")     # 3 line
+    print(line_space + "\n" * 5)
+
+
+def place_marker(board, marker, position):
+    """
+        Placing marker in postion on the board
+    """
+    board[position] = marker
+
+
+def space_check(board, position):
+    """
+        Checking - where a space on the board is freely available
+    """
+    return board[position] == ' '
+
+
+def full_board_check(board):
+    """
+        Checking - if the board is a full and returns a boolean valie. True = full, False - otherwise
+    """
+    count = 0
+    for item in range(1,10):
+        if board[item] != ' ':
+            count += 1
+    return count == 9    
+
+
+def win_check(board, marker):
+    """
+        Checking - in each move, do someone win
+    """
+    return ((board[1] == marker and board[2] == marker and board[3] == marker) or
+           (board[4] == marker and board[5] == marker and board[6] == marker) or
+           (board[7] == marker and board[8] == marker and board[9] == marker) or
+           (board[1] == marker and board[4] == marker and board[7] == marker) or
+           (board[2] == marker and board[5] == marker and board[8] == marker) or
+           (board[3] == marker and board[6] == marker and board[9] == marker) or
+           (board[1] == marker and board[5] == marker and board[9] == marker) or
+           (board[3] == marker and board[5] == marker and board[7] == marker))
+
+
+def choose_first():
+    """
+        Randomly choose, which player will be first
+    """
+    from random import randint
+    first_player = randint(0,1)
+    if first_player == 0:
+        return 'Player 1'
+    else:
+        return 'Player 2'
+
+
+def player_input():
+    """
+        Input function - move of players (markers - X or O, and place in range from 1 to 9)
+    """
+    marker = ''
+    
+    while not (marker == 'X' or marker == 'O'):
+        marker = input('Player 1: Do you want to be X or O?').upper()
+    
+    if marker == 'X':
+        return ('X', 'O')
+    else:
+        return ('O', 'X')
+
+
+def player_choice(board):
+    """
+        It's ask user to input position of his move and return that position
+    """
+    position = 0
+    while position not in [1, 2, 3, 4, 5, 6, 7, 8, 9] or not space_check(board, position):
+        position = int(input("Choose your next position (1 - 9): "))
+
+    return position
+
+
+
+def start_game(board):
+    """
+        Main game loop
+    """
+    game_on = True
+    print("Welcome! Let's play")
+    input()
+    player1_marker, player2_marker = player_input()
+    turn = choose_first()
+
+    while game_on:
+        
+        if turn == 'Player 1':
+            board_display(board)
+            print("Player 1 move.")
+            move = player_choice(board)
+            place_marker(board, player1_marker, move)
+            if win_check(board, player1_marker):
+                board_display(board)
+                print("Player 1 win!")
+                break
+            turn = 'Player 2'
+
+        else:
+            board_display(board)
+            print("Player 2 move.")
+            move = player_choice(board)
+            place_marker(board, player2_marker, move)
+            if win_check(board, player2_marker):
+                
+                print("Player 2 win!")
+                break
+            turn = 'Player 1'
+
+start_game(newGame)
