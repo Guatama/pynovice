8052a12b7d3d73698af695f6f47b753ff1dc53f4
Guatama <flatline.mail@gmail.com>
2019-09-26 16:24:17 +0500

    little revesion

--- dice.py
+++ dice.py
@@ -1,24 +1,18 @@
 __author__ = 'Flatline01'
+from random import randint
+
 # That programm simulates the roll of two dice and display the result (sum of points).
+if __name__ == '__main__':
+    answer = ["no", "n", "quit", "bye", "exit", "q"]
+    while True:   
+        dice1 = randint(1, 6)
+        dice2 = randint(1, 6)
+        result = dice1 + dice2
 
-print("With this program you throw two dice!")
+        print("--------------------------")
+        print(f"Dice: {dice1}\t\tDice: {dice1}\nYour points: {result}")
+        print("--------------------------")
 
-# The random number generator throws two dice
-while True:
-    from random import randint
-    dice11 = randint(1, 6)
-    dice12 = randint(1, 6)
-    result1 = dice11 + dice12
-
-    print("--------------------------")
-    print("First Dice:", dice11, "\t\tSecond Dice:", dice12, "\n\t\tYour points:", result1)
-
-
-# The interface to end the program
-    answer1 = ["no", "n", "quit", "bye", "exit"]
-    choose = str(input("\nWant to continue? (enter/no)"))
-
-    # Does the case insensitive
-    choose = choose.lower()
-    if choose in answer1:
-        break
+        choose = str(input("\nWant to continue? (enter/no)")).lower()
+        if choose in answer or choose.isdigit():
+            break
