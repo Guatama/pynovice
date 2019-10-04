d58b1f1d417daa5e6676f0ecf4523561ca85c93d
Guatama <flatline.mail@gmail.com>
2019-10-04 11:14:29 +0500

    Some little changes in dice.py

--- dice.py
+++ dice.py
@@ -1,18 +1,20 @@
 __author__ = 'Flatline01'
-from random import randint
+# import random
+import numpy as np
 
 # That programm simulates the roll of two dice and display the result (sum of points).
 if __name__ == '__main__':
     answer = ["no", "n", "quit", "bye", "exit", "q"]
     while True:   
-        dice1 = randint(1, 6)
-        dice2 = randint(1, 6)
+        # dice1 = random.randint(1, 6)
+        # dice2 = random.randint(1, 6)
+        dice1, dice2 = np.random.randint(1, 7, 2)
         result = dice1 + dice2
 
-        print("--------------------------")
-        print(f"Dice: {dice1}\t\tDice: {dice1}\nYour points: {result}")
+        print("\n\n--------------------------")
+        print(f"Dice: {dice1}\t\tDice: {dice2}\nYour points: {result}")
         print("--------------------------")
 
-        choose = str(input("\nWant to continue? (enter/no)")).lower()
+        choose = str(input("Want to continue? (enter/no)")).lower()
         if choose in answer or choose.isdigit():
             break
