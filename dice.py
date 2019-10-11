8e64044a33b723bd9980f7518deda2c64f2ee109
Guatama <flatline.mail@gmail.com>
2019-10-11 13:48:08 +0500

    After flake

--- dice.py
+++ dice.py
@@ -2,10 +2,10 @@ __author__ = 'Flatline01'
 # import random
 import numpy as np
 
-# That programm simulates the roll of two dice and display the result (sum of points).
+# Code simulates the roll of two dice and display the result (sum of points).
 if __name__ == '__main__':
     answer = ["no", "n", "quit", "bye", "exit", "q"]
-    while True:   
+    while True:
         # dice1 = random.randint(1, 6)
         # dice2 = random.randint(1, 6)
         dice1, dice2 = np.random.randint(1, 7, 2)
--- tictactoe_game.py
+++ tictactoe_game.py
@@ -1,15 +1,17 @@
 __author__ = 'Flatline01'
 __version__ = '1.0.0'
 
-import os, platform
+import os
+import platform
 
 testGame = ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
 newGame = ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
+
 def clear_screen(JupyterNotebook=False):
     """
         Clear screen in terminal. With JupyerNotebook == True - did it for that enviroment with IPython function
-    """ 
+    """
     if JupyterNotebook:
         from IPython.display import clear_output
         clear_output()
@@ -30,13 +32,19 @@ def board_display(board):
 
     clear_screen()
     print("\n\n")
-    print(line_space + "\n" + spaces + " " + board[1] + "  | " + board[2] + " | " + board[3] + " ")     # 1 line
-    print(line_space + "\n" + line_horiz)
-    print(line_space + "\n" + spaces + " " + board[4] + "  | " + board[5] + " | " + board[6] + " ")     # 2 line
-    print(line_space + "\n" + line_horiz)
-    print(line_space + "\n" + spaces + " " + board[7] + "  | " + board[8] + " | " + board[9] + " ")     # 3 line
-    print(line_space + "\n" * 5)
+    print("{}\n{} {} | {} | {} ".format(line_space, spaces, board[1], board[2], board[3]))
+    print("{}\n{}".format(line_space, line_horiz))
+    print("{}\n{} {} | {} | {} ".format(line_space, spaces, board[1], board[2], board[3]))
+    print("{}\n{}".format(line_space, line_horiz))
+    print("{}\n{} {} | {} | {} ".format(line_space, spaces, board[1], board[2], board[3]))
 
+
+    # print(line_space + "\n" + spaces + " " + board[1] + "  | " + board[2] + " | " + board[3] + " ")     # 1 line
+    # print(line_space + "\n" + line_horiz)
+    # print(line_space + "\n" + spaces + " " + board[4] + "  | " + board[5] + " | " + board[6] + " ")     # 2 line
+    # print(line_space + "\n" + line_horiz)
+    # print(line_space + "\n" + spaces + " " + board[7] + "  | " + board[8] + " | " + board[9] + " ")     # 3 line
+    print(line_space + "\n" * 5)
 
 
 def place_marker(board, marker, position):
@@ -58,10 +66,10 @@ def full_board_check(board):
         Checking - if the board is a full and returns a boolean valie. True = full, False - otherwise
     """
     count = 0
-    for item in range(1,10):
+    for item in range(1, 10):
         if board[item] != ' ':
             count += 1
-    return count == 9    
+    return count == 9
 
 
 def win_check(board, marker):
@@ -87,12 +95,13 @@ def draw_check(board, marker_1, marker_2):
         return True
     return False
 
+
 def choose_first():
     """
         Randomly choose, which player will be first
     """
     from random import randint
-    first_player = randint(0,1)
+    first_player = randint(0, 1)
     if first_player == 0:
         return 'Player 1'
     else:
@@ -102,8 +111,8 @@ def choose_first():
 def dialog_YN(question):
     """
         Func for questions with bool answer. Return TRUE if Yes, and FALSE if No
-    """   
-    list_of_answer = ['y','yes','n','no',]
+    """
+    list_of_answer = ['y', 'yes', 'n', 'no']
     answer = ''
     while answer not in list_of_answer:
         answer = input(question).lower()  
@@ -121,10 +130,10 @@ def player_input():
         Input function - move of players (markers - X or O, and place in range from 1 to 9)
     """
     marker = ''
-    
+
     while not (marker == 'X' or marker == 'O'):
         marker = input('Player 1: Do you want to be X or O?  ').upper()
-    
+
     if marker == 'X':
         return ('X', 'O')
     else:
@@ -139,9 +148,10 @@ def player_choice(board):
     while position not in [1, 2, 3, 4, 5, 6, 7, 8, 9] or not space_check(board, position):
         try:
             position = int(input("Choose your next position (1 - 9): "))
-        except:
+        except Exception:
             continue
     return position
+
 
 def replay():
     """
@@ -158,17 +168,18 @@ def start_game(board):
     print("\n\n-----Welcome to Tic Tac Toe!------")
     print("--------------ver 1.0------------")
     print("=" * 40)
-    
+
     while True:
-           
         theBoard = [' '] * 10
         player1_marker, player2_marker = player_input()
         turn = choose_first()
         print("\n\n" + turn + " will go first.")
-        # TODO do alter function for replay and all YES and NO questions with question(str) --> str + Y/N and Bool
-        game_on = dialog_YN("Are you ready to play? Enter YES or NO -  ")   
+
+        # TODO do alter function for replay.
+        # All YES and NO questions with question(str) --> str + Y/N and Bool
+        game_on = dialog_YN("Are you ready to play? Enter YES or NO -  ")
+
         while game_on:
-            
             if turn == 'Player 1':
                 board_display(theBoard)
                 print("Player 1 move.")
@@ -196,6 +207,7 @@ def start_game(board):
                 turn = 'Player 1'
         if not replay():
             break
-            
+
+
 if __name__ == '__main__':
     start_game(newGame)
