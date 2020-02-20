3c082a4391c1c7e994d65387259b70c429ecbc50
Guatama <flatline.mail@gmail.com>
2020-02-20 13:17:27 +0500

    Little fixes for binary_search_game for terminal

--- Binary Search Game/binary_search_game.py
+++ Binary Search Game/binary_search_game.py
@@ -2,30 +2,32 @@
 The game of guessing the number in the specified range, with the implementation of the binary search algorithm.
 """
 __author__ = 'Flatline01'
-import math
+from math import log
+
 
 x_range = 1
 y_range = 100
 last_guess_number = 0
 
 effort_number = 0
-efforts = math.log(y_range, 2)
+efforts = log(y_range, 2)
+
 print(f"I will guess the number in no more than {round(efforts)} attempts.")
 
 while x_range <= y_range:
-    # last_guess_number = (x_range + y_range) / 2 - this is formula for binary search too
+    # (x_range + y_range) // 2 - this is formula for binary search too
     last_guess_number = x_range + (y_range - x_range) / 2
-    print(f"I guess it's {round(last_guess_number)}?")
-    answer = input("Answer: ")
-    
+    print(f"\nI guess it's {round(last_guess_number)}?")
+    answer = input("Less, More or Yes > ")
+
     if effort_number > efforts:
-        print(f"Correct answer is {last_guess_number}! Don't fool me!")
+        print(f"\nCorrect answer is {round(last_guess_number)}! Don't fool me!")
         break
 
     if answer.lower() == "yes":
-        print(f"Correct! It's {round(last_guess_number)}")
+        print(f"\nCorrect! It's {round(last_guess_number)}")
         break
-        
+
     elif answer.lower() == "more":
         effort_number += 1
         print(f"It's effort number {effort_number}. Try again!")
@@ -35,4 +37,3 @@ while x_range <= y_range:
         effort_number += 1
         print(f"It's effort number {effort_number}. Try again!")
         y_range = last_guess_number
-
