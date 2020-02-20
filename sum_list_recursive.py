17fa809cbbefe9795259557fa2434b7089b3e76b
Guatama <flatline.mail@gmail.com>
2020-02-20 18:00:09 +0500

    .

--- sum_list_recursive.py
+++ sum_list_recursive.py
@@ -2,29 +2,34 @@
     Here I study recursion in programming.
 """
 
+
 # sum function with for-loop
-def sum(list):
+def sum(input_list):
     sum_result = 0
-    for item in list:
+    for item in input_list:
         sum_result += item
     return sum_result
 
+
 # sum function with recursion
-def sum_rec(list):
-    if list == []:
+def sum_rec(input_list):
+    if len(input_list) == 0:
         return 0
     else:
-        return list[0] + sum_rec(list[1:])
+        return input_list[0] + sum_rec(input_list[1:])
+
 
 # counter of items in list with recursion 
-def count_rec(list):
-    if list == []:
+def count_rec(input_list):
+    if len(input_list) == 0:
         return 0
     else:
-        return 1 + count_rec(list[1:])
+        return 1 + count_rec(input_list[1:])
+
 
 if __name__ == '__main__':
     testin_l = [1, 24, 6, 234, 340, 3, 0, 2]
+    print(testin_l)
     print("testing:", 1 + 24 + 6 + 234 + 340 + 3 + 0 + 2)
     print(sum(testin_l))
     print(sum_rec(testin_l))
